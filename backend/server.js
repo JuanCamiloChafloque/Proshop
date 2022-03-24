dde434f2eb0047c9bc6de3fb186112b35b3cae6f
Camilo <camilochafloque@gmail.com>
2022-03-24 09:59:52 -0500

    Added concurrency to backend and frontend starts

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const products = require("./data/products");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/api/products", (req, res) => {
   res.status(200).json(products);
@@ -14,4 +17,9 @@ app.get("/api/products/:id", (req, res) => {
   res.status(200).json(product);
 });
 
-app.listen(PORT, console.log("Server running on port " + PORT));
+app.listen(
+  PORT,
+  console.log(
+    "Server running on " + process.env.NODE_ENV + " mode on port " + PORT
+  )
+);
